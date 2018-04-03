583430ae577c6449b2b264b3488665f5ec69f94a
武斌 <wubin_oct@163.com>
2018-04-03 16:27:26 +0800

    小改

---
+++ common/common.js
@@ -0,0 +1,18 @@
+function  show_WxImg(){
+  document.getElementById("wxImg").style.visibility='visible';
+}
+function hide_WxImg(){
+  document.getElementById("wxImg").style.visibility='hidden';
+}
+function  show_qqImg(){
+  document.getElementById("qqImg").style.visibility='visible';
+}
+function hide_qqImg(){
+  document.getElementById("qqImg").style.visibility='hidden';
+}
+function show(){
+  document.getElementById("sec-adv").style.visibility='visible';
+}
+function hide(){
+  document.getElementById("sec-adv").style.visibility='hidden';
+}
